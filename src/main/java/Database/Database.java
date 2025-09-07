5d38ea092f775bc90aa144eba263342a4fd6b849
Arnaldur <146443649+Add1Padd1@users.noreply.github.com>
2025-09-07 15:13:16 +0000

    Bara mitt fyrsta commit

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,5 +1,5 @@
 package Database;
-/*Þarna ætti kannski databaseið að vera, breytum seinna - Arnaldur*/
+/*Þarna ætti kannski databaseið að vera, munum breyta seinna - Arnaldur*/
 public class Database {
     public static void main(String[] args) {
 
