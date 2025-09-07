f98f00526c86ab2d5939717df1f633048fcd050e
AronBjartur <abh41@hi.is>
2025-09-07 14:04:10 +0000

    setup

---
+++ src/main/java/is/hi/hbv501g/team_19_bank/Team19BankApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.team_19_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team19BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team19BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/team_19_bank/Team19BankApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.team_19_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team19BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
